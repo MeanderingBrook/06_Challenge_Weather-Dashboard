313d3c627945d2d328640097eda33e0bfb995955
Meandering Brook <jaguar_rebuke_0g@icloud.com>
2024-07-02 13:51:10 -0400

    README Screenshots

--- env-local.js
+++ env-local.js
@@ -1,6 +1,6 @@
-// // Defines Developer-specific OpenWeather API Key
+// Defines Developer-specific OpenWeather API Key
 // const API_KEY = "91512c0479417b46b9d5f9b5e91a4f51";
-// // console.log(API_KEY);
+// console.log(API_KEY);
 
 // module.exports = { API_KEY };
 
