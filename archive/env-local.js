73ed2270df4ab44f097db89874d941dcfa1f206c
Meandering Brook <jaguar_rebuke_0g@icloud.com>
2024-07-05 10:33:09 -0400

    README update, misc code clean-up, env-local to Archive

--- archive/env-local.js
+++ archive/env-local.js
@@ -1,3 +1,6 @@
+// Note to Reviewers: This code is unused and is the result of an experiment to duplicate React.js functionality
+// that places sensitive information in a secure location (e.g., env-local file)
+
 // Defines Developer-specific OpenWeather API Key
 // const API_KEY = "91512c0479417b46b9d5f9b5e91a4f51";
 // console.log(API_KEY);
